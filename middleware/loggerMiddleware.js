37fed93d80e5459b1000b9fbb52b1fc1851b6d22
hamza-cominity <hamza@cominity.ca>
2023-01-21 01:55:28 +0100

    Got rid of js files

--- middleware/loggerMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var loggerMiddleware = function (req, res, next) {
-    console.log("req body", req.body);
-    console.log("req params", req.params);
-    console.log("req query", req.query);
-    next();
-};
-exports["default"] = loggerMiddleware;
