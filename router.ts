6a84aebb88d3453dbe3be793c731598b005aaa87
hamza-cominity <hamza@cominity.ca>
2023-01-21 01:29:59 +0100

    Added a simple hello

--- router.ts
+++ router.ts
@@ -1,8 +1,11 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import userRouter from "./elements/user/user.router";
 
 const router = Router();
 
 router.use("/users", userRouter);
+router.use("/", (req: Request, res: Response) => {
+  res.status(200).send("Hello!");
+});
 
 export default router;
