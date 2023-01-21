29bd1c922b8475eacbaeec617bbc388106bd090f
hamza-cominity <hamza@cominity.ca>
2023-01-21 02:16:04 +0100

    Added mongo url to logger

--- middleware/loggerMiddleware.ts
+++ middleware/loggerMiddleware.ts
@@ -4,6 +4,8 @@ const loggerMiddleware = (req: Request, res: Response, next: NextFunction) => {
   console.log("req body", req.body);
   console.log("req params", req.params);
   console.log("req query", req.query);
+  //@ts-ignore
+  console.log("mongo url", process.env.MONGO_URI);
 
   next();
 };
