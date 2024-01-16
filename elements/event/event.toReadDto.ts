1e314ef4042e6d0a947949bca4d1c856343921c9
Hamza <hamza.khalifa.clashingames@gmail.com>
2024-01-16 04:57:24 +0100

    Field micro frontend

--- elements/event/event.toReadDto.ts
+++ elements/event/event.toReadDto.ts
@@ -1,8 +1,18 @@
 import { IEventReadDto } from "roottypes";
 import { IEvent } from "./event.model";
 import { microFrontendToReadDto } from "../microFontend/microFrontend.toReadDto";
+import mongoose from "mongoose";
 
-export const eventToReadDto = (event: IEvent): IEventReadDto => {
+export const eventToReadDto = (
+  event: IEvent | string
+): IEventReadDto | string => {
+  if (
+    typeof event === "string" ||
+    mongoose.Types.ObjectId.isValid(event.toString())
+  ) {
+    return event.toString();
+  }
+
   return {
     eventTrigger: event.eventTrigger,
     eventType: event.eventType,
